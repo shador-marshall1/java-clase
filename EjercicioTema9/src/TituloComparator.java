3a0d2b06a4b41bbaa3ffb2746c5e02eed2ed999f
DAM1ALU13 <DAM1ALU13@B01-PC06.IESGUARA.LOCAL>
2021-02-08 13:08:47 +0100

    pene

---
+++ EjercicioTema9/src/TituloComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+public class TituloComparator {
+
+class TituloComparator implements Comparator <Libro> {
+public int compare (Libro l1, Libro l2) {
+	return l1.dameTitulo().compareTo(l2.Dametitulo)
+}
+	
+
+}
