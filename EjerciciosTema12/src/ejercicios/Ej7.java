b6e1a87a9828bd991c84e25e6904d3bfd164cf9f
shador-marshall1 <180170@iessierradeguara.com>
2021-03-31 16:22:59 +0200

    sdfsafd

---
+++ EjerciciosTema12/src/ejercicios/Ej7.java
@@ -0,0 +1,30 @@
+package ejercicios;
+
+import java.io.*;
+
+
+public class Ej7 {
+
+	public static void main(String[] args) throws IOException {
+		try {
+			FileOutputStream fileout = new FileOutputStream("ficheros/Ej7.dat");
+			DataOutputStream dataOS=new DataOutputStream(fileout);
+			double numeroAleatorio = (double) (Math.random()*23+1);
+			String dia [] = {"10 Agosto 2019"};
+			Double temperaturas [] = {numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio,};
+			for (int i=0; i< dia.length; i++) {
+				dataOS.writeUTF(dia[i]);
+			}
+			for (int i =0; i < temperaturas.length; i++) {
+				dataOS.writeDouble(temperaturas[i]);
+		
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
---
+++ EjerciciosTema12/src/ejercicios/Ej8.java
@@ -0,0 +1,26 @@
+package ejercicios;
+
+import java.io.*;
+
+public class Ej8 {
+
+	public static void main(String[] args) {
+		try {
+		FileInputStream filein = new FileInputStream("ficheros/Ej7.dat");
+		DataInputStream dataIS=new DataInputStream(filein);
+		String n;
+		Double d;
+		while (true) {
+			n=dataIS.readUTF();
+			d=dataIS.readDouble();
+			System.out.println("El día "+n+ " Hará las temperaturas: "+d);
+		
+		}
+		}
+		catch (IOException e){
+			e.printStackTrace();
+			
+		}
+	}
+
+}
