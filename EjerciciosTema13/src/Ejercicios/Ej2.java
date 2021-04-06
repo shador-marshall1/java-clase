c5a33b004b17423ad3d1f91bcbec2756a6fc2ad5
shador-marshall1 <180170@iessierradeguara.com>
2021-04-06 14:16:32 +0200

    asdfas

---
+++ EjerciciosTema13/src/Ejercicios/Ej2.java
@@ -0,0 +1,43 @@
+package Ejercicios;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Ej2 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ej2 window = new Ej2();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Ej2() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+}
