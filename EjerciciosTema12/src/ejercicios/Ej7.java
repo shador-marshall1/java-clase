aec74a229bbf896451b0a57f1702ffd7b3c11ced
shador-marshall1 <180170@iessierradeguara.com>
2021-04-06 13:17:16 +0200

    sefse

--- EjerciciosTema12/src/ejercicios/Ej7.java
+++ EjerciciosTema12/src/ejercicios/Ej7.java
@@ -4,14 +4,21 @@ import java.io.*;
 
 
 public class Ej7 {
-
+	private static double numeroAleatorio () {
+		Double temperaturas=0.0;
+		for (int i=0; i<24;i++) {
+		temperaturas = (Double) (Math.random()*23+1);
+		
+		}
+		return temperaturas;
+	}
 	public static void main(String[] args) throws IOException {
 		try {
 			FileOutputStream fileout = new FileOutputStream("ficheros/Ej7.dat");
 			DataOutputStream dataOS=new DataOutputStream(fileout);
-			double numeroAleatorio = (double) (Math.random()*23+1);
+			double numeroAleatorio = numeroAleatorio();
 			String dia [] = {"10 Agosto 2019"};
-			Double temperaturas [] = {numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio, numeroAleatorio,};
+			Double temperaturas [] = {numeroAleatorio(), numeroAleatorio(), numeroAleatorio()};
 			for (int i=0; i< dia.length; i++) {
 				dataOS.writeUTF(dia[i]);
 			}
@@ -19,7 +26,7 @@ public class Ej7 {
 				dataOS.writeDouble(temperaturas[i]);
 		
 			}
-			
+			System.out.println(Math.round(numeroAleatorio()) +" "+Math.round(numeroAleatorio()));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
