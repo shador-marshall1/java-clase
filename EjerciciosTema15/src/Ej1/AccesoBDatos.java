314d611d512bf018887960970f84a37a4559edf8
shador-marshall1 <180170@iessierradeguara.com>
2021-04-20 17:31:22 +0200

    zxvc

--- EjerciciosTema15/src/Ej1/AccesoBDatos.java
+++ EjerciciosTema15/src/Ej1/AccesoBDatos.java
@@ -9,6 +9,7 @@ public class AccesoBDatos {
 	    private static String username = "root";
 	    private static String password = "root";
 	    public Connection conecta;
+	    public ResultSet rst2;
 
 		public void conectar() throws SQLException, ClassNotFoundException {
 			Class.forName(driver);
@@ -18,40 +19,54 @@ public class AccesoBDatos {
 			try {
 				Statement consulta = conecta.createStatement();
 				if(localidad!=null) {
-				ResultSet rst = consulta.executeQuery("SELECT LOCALIDAD FROM SOCIO WHERE LOCALIDAD LIKE'%"+localidad+"%'");
+				ResultSet rst = consulta.executeQuery("SELECT NOMBRE, LOCALIDAD FROM SOCIO WHERE LOCALIDAD LIKE'%"+localidad+"%'");
 				while (rst.next()) {
+					String Nombre = rst.getString("Nombre");
 					String Localidad = rst.getString("localidad");
-					System.out.println(Localidad);
-					if (rst.last()==true) {
-						System.out.println("Total de socios con esa localidad: "+rst.getRow());
+					System.out.println(Localidad +"\t" +Nombre);
+					
 					}
+				if (rst.last()==true) {
+					System.out.println("Total de socios con esa localidad: "+rst.getRow());
 				}
 				}
 				else if (localidad==null) {
 					ResultSet rst1= consulta.executeQuery("select LOCALIDAD from SOCIO");
 					while (rst1.next()) {
+						String Nombre1 = rst1.getString("Nombre");
 						String Localidad1 = rst1.getString("Localidad");
 						System.out.println(Localidad1);
-						System.out.println("Socios con localidad; "+Localidad1);
-						if (rst1.last()==true) {
-							System.out.println("Socios totales: "+rst1.getRow());
+						System.out.println("Socios con localidad; "+Localidad1+ "\t" +Nombre1);
+						
 						}
-						}
+					if (rst1.last()==true) {
+						System.out.println("Socios totales: "+rst1.getRow());
+					}
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
+		public ResultSet resultSetPrueba () throws SQLException {
+			Statement consulta1 = conecta.createStatement();
+			ResultSet prueba1 = consulta1.executeQuery("Select * from socio");
+			this.rst2 = prueba1;
+			return rst2;
+		}
+		
 		public void imprimirDatos (ResultSet r) throws SQLException {
 			ResultSet consulta1 = r;
-			while (consulta1.next()) {
+			while (r.next()) {
 				int SocioID = consulta1.getInt(1);
 				String Nombre = consulta1.getString(2);
 				int Estatura = consulta1.getInt(3);
 				int Edad = consulta1.getInt(4);
 				String Localidad = consulta1.getString(5);
+				System.out.println(SocioID +"\t"+Nombre + "\t" + Estatura+ "\t" +Edad+ "\t" +Localidad);
 			}
-			
+			if (r.last()==true) {
+				System.out.println("Total de filas: "+r.getRow());
+			}
 			
 		}
 		public void desconectar() throws SQLException {
@@ -59,7 +74,11 @@ public class AccesoBDatos {
 				conecta.close();
 			}
 		}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception, SQLException { 
+		AccesoBDatos bd = new AccesoBDatos();
+		bd.conectar();
+		bd.consultaPorLocalidad("Malaga");
+		bd.imprimirDatos(bd.resultSetPrueba());
 		
 
 	}
